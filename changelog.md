5c9e5ab81314a787fd02ce885a491f332bf36a83
Julien <julien.seixas@free.fr>
2020-01-04 23:44:22 +0100

    V3.0

--- changelog.md
+++ changelog.md
@@ -6,11 +6,28 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
-## [2.1.0] - 2019-12-17
+## [3.0.0] - 2020-01-03
+### Added
+- Add git files & postgres-persistent-data folder to dockerignore
+- Add postgres-persistent-data folder to gitignore
+- Add new env key to nodeapi container
+- Add Volume to persist postgres data
+- Add `pg` & `redis` npm package
+- Add `compose-up-prod`, `compose-up-dev`, `compose-down-prod` and `compose-down-dev`. Please read the README.md to know what they did
+- Add **Redis** & **Postgres** client on API
+
+### Changed
+- Update docker-compose.prod according to docker-compose.dev
+
+### Removed
+- Remove unused ENV keys in Dockerfile of nodeapi container
+
+## [2.1.0] - 2020-01-03
 ### Changed
 - adapt dev command to be usable on any OS localy
 
-## [2.0.0] - 2019-12-17
+
+## [2.0.0] - 2020-01-03
 ### Added
 - Add `start` command in package.json for production launch
 - Add `dev` command in package.json for developpement launch
@@ -29,14 +46,17 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Change nginx configuration according to new nodeapi port (3030)
 - Change all docker images to alpine version
 
+
 ## [1.0.0] - 2019-12-17
 ### Added
 - Create Dockerfile for reverse proxy (nginx)
 - Add nginx conf for api reverse proxy
 
+
 ## [0.4.0] - 2019-12-17
 ### Added
 - Add ignore files
+
 
 ## [0.3.0] - 2019-12-17
 ### Added
@@ -45,9 +65,11 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Removed
 - virtualbox docker tutorial for windows family in readme
 
+
 ## [0.2.0] - 2019-12-17
 ### Changed
 - Add virtualbox docker tutorial for windows family
+
 
 ## [0.1.0] - 2019-12-17
 ### Added
--- server.js
+++ server.js
@@ -1,30 +1,59 @@
 'use-strict'
 
+// Create instances
 const Express = require('express');
 const WebServer = Express();
 const PORT = process.env.API_PORT || 3030;
+const { Client } = require('pg')
+const redis = require("redis")
 
+
+// Setup instances
+const pgClient = new Client({
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+})
 
 WebServer.listen(PORT, (error) => {
     if(error) {
         throw error;
     }
 
+    pgClient.connect()
+
     console.log('## API STARTED !')
-    console.log(`## Express server listening on port ${PORT}`);
+    console.log(`## Express server listening on local container port ${PORT}`);
 });
 
+const redisClient = redis.createClient({ host: process.env.REDIS_HOST });
+
+//
 WebServer.get('/', function (req, res)
 {
     res.json({ message: "Hello World" });
 });
 
 
-WebServer.get('/status', function (req, res)
-{
+WebServer.get('/status', async (req, res) => {
+    const postgresQuery = 'SELECT date_trunc(\'second\', current_timestamp - pg_postmaster_start_time()) as uptime;'
+    const result = await pgClient.query(postgresQuery)
+    const uptime = result.rows[0].uptime
+    const uptimeString = () => {
+        let time = ''
+
+        time += uptime.hours ? `${uptime.hours}h ` : ''
+        time += uptime.minutes ? `${uptime.minutes}m ` : ''
+        time += uptime.seconds ? `${uptime.seconds}s` : ''
+
+        return time
+    }
+
     res.json({
-        "status": "OK",
-        "postgresUptime": String,
-        "redisConnectedClients": Number
-        });
-});
+        status: 'ok',
+        postgresUptime: uptimeString(),
+        redisConnectedClients: Number(redisClient.server_info.connected_clients)
+    })
+})
